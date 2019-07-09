b90b7d693956a368f9aead77cc029194f8d53983
MJ <marcusjones@hotmail.com>
2019-07-09 21:21:33 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,58 @@
+provider "vsphere" {
+  user           = "administrator@sso.domain"
+  password       = "100%foVGA"
+  vsphere_server = "192.168.1.219"
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
+}
+
+data "vsphere_datacenter" "dc" {
+  name = "MJ-Datacenter"
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = "datastore1"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_datastore" "iso_datastore" {
+  name          = "datastore1"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "MJ-Resource-Pool"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_network" "network" {
+  name          = "VM Network"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "other3xLinux64Guest"
+
+  wait_for_guest_net_timeout = "0"
+
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+  }
+
+ cdrom {
+    datastore_id = "${data.vsphere_datastore.iso_datastore.id}"
+    path = "iso/Core-current.iso"
+  }
+}
