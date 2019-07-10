a63cb8e0163d017d437a9a51d5bf42e7bd709e8f
MJ <marcusjones@hotmail.com>
2019-07-10 21:50:45 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,33 +10,3 @@ variable "vcenter" {
 variable "datacenter" {
   type = "string"
 }
-variable "datastore" {
-  type = "string"
-}
-variable "iso_datastore" {
-  type = "string"
-}
-variable "pool" {
-  type = "string"
-}
-variable "network" {
-  type = "string"
-}
-variable "vmname" {
-  type = "string"
-}
-variable "numcpus" {
-  type = "string"
-}
-variable "timeout" {
-  type = "string"
-}
-variable "disk_label" {
-  type = "string"
-}
-variable "size" {
-  type = "string"
-}
-variable "iso" {
-  type = "string"
-}
