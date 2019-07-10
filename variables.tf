7cd0167694a5afc4bb995ea22aa4c6ba77558f40
MJ <marcusjones@hotmail.com>
2019-07-10 19:45:24 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,42 @@
+variable "username" {
+  type = string
+}
+variable "password" {
+  type = string
+}
+variable "vcenter" {
+  type = string
+}
+variable "datacenter" {
+  type = string
+}
+variable "datastore" {
+  type = string
+}
+variable "iso_datastore" {
+  type = string
+}
+variable "pool" {
+  type = string
+}
+variable "network" {
+  type = string
+}
+variable "vmname" {
+  type = string
+}
+variable "numcpus" {
+  type = number
+}
+variable "timeout" {
+  type = number
+}
+variable "disk_label" {
+  type = string
+}
+variable "size" {
+  type = number
+}
+variable "iso" {
+  type = string
+}
