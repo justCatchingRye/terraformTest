2dab6d73f3fd17d0444b2814ac65d36322aa101c
MJ <marcusjones@hotmail.com>
2019-07-11 07:06:09 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,12 @@
+variable "vsphere_user" {
+  type = "string"
+}
+variable "vsphere_pw" {
+  type = "string"
+}
+variable "vsphere_addr" {
+  type = "string"
+}
 variable "vmname" {
   type = "string"
 }
